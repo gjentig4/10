25f0a0b20d740e100ad6b916ad4f9a2c4151a392
Gent <gjentig4@gmail.com>
2021-12-10 23:42:19 +0200

    update

--- main.cpp
+++ main.cpp
@@ -22,80 +22,95 @@ class LimitedCounter:public Counter{
 public:
     LimitedCounter(int lower = 0, int upper = 0);
     void inc() override{
-        count++;
+        if (count < upper){
+            count++;}
     }
     void dec() override{
-        count--;
+        if (count > 0){
+            count--;
+        }
     }
-    operator int () override {
+    operator int() override{
         return count;
     }
 private:
     int count;
-    int limit;
+    int upper;
 };
 
-LimitedCounter::LimitedCounter(int lower, int upper):Counter() {
+LimitedCounter::LimitedCounter(int lower, int upperL):Counter() {
  count = lower;
- limit = upper;
+ upper = upperL;
 }
 
-/*
-class OverflowCounter :public Counter {
+class OverflowCounter:public Counter{
 public:
-    OverflowCounter (int lower = 0, int upper = 0);
-    void inc() override{
-        if (count < limit){
+    OverflowCounter (int init = 0, int upperL = 0);
+    void inc() override {
+        if (count < upper)
             count++;
-        }
 
-    else {
-        count =0;
+        else if (count == upper)
+            count = 0;
+
+        else if (count == 0)
+            count++;}
+void dec() override{
+        if (count > 0)
+            count--;
+
+        else if (count == 0)
+            count = upper;
+
+        else if (count == upper)
+            count--;
     }
+    operator int() override {
+            return count;
+        }
 private:
     int count;
-    int limit;
+    int upper;
 };
+    OverflowCounter::OverflowCounter(int init, int upperL): Counter(){
+        count = init;
+        upper = upperL;
+    }
 
-OverflowCounter::OverflowCounter(int lower, int upper):Counter() {
-    count = lower;
-    limit = upper;
+void UseCounter(Counter& ctr, int num) {
+    int i = 0;
+    if (num > 0) {
+        while(i<num){
+            ctr.inc();
+            i++;
+        }
+    }
+    else {
+        while (i<-(num)){
+            ctr.dec();
+            i++; }
+    }
 }
-*/
-
-
-
-
-void UseCounter(Counter& ctr, int num){
-
-};
-
 int main(int argc, char** argv) {
-/*    LimitedCounter lc(0, 5);
-    //OverflowCounter oc(5, 9);
-
-    cout << oc << endl;
-    UseCounter(2, 5);
-    cout << oc << endl; // should display zero
-
-    oc.dec();
-    cout << oc << endl; // should display 8
-
-    cout << lc << endl;
-    lc.inc();
-    cout << lc << endl;
-    lc.dec();
-    cout << lc << endl;
-    for(int i = 0; i < 10; ++i) lc.inc();
-    cout << lc << endl;
-    UseCounter(lc, -9);
-    cout << lc << endl;*/
-
     LimitedCounter lc(0, 5);
+    OverflowCounter oc(5, 9);
 
-    lc.operator int();
+    cout << oc << endl; //5
+    UseCounter(oc, 5);
+    cout << oc << endl;  //0
+    UseCounter(oc, -1);
+    cout << oc << endl; //9
+    oc.dec();
+    cout << oc << endl; //8
+
+    cout << lc << endl; // 0
     lc.inc();
-    cout << lc << endl; // should display 9
-
+    cout << lc << endl; //1
+    lc.dec();
+    cout << lc << endl; //0
+    for(int i = 0; i < 10; ++i) lc.inc();
+    cout << lc << endl; //5
+    UseCounter(lc, -9);
+    cout << lc << endl; //0
     return 0;
 }
